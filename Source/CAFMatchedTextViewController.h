11cfa843c03088ca16407561e5f68b56cbb74a67
Matt Thomas <matt@codecaffeine.com>
2012-08-26 08:55:12 -0400

    Add CAFMatchedTextViewController

---
+++ Source/CAFMatchedTextViewController.h
@@ -0,0 +1,13 @@
+//
+//  CAFMatchedTextViewController.h
+//  Curiosity
+//
+//  Created by Matthew Thomas on 8/26/12.
+//  Copyright (c) 2012 Matthew Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAFMatchedTextViewController : UIViewController
+
+@end
