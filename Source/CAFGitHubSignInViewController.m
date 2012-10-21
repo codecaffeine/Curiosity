7a61faa5cb9a42ae59e188ec6a10aa66f013aef5
Matt Thomas <matt@codecaffeine.com>
2012-10-21 15:22:52 -0400

    Add methods to send basic authentication via header

--- Source/CAFGitHubSignInViewController.m
+++ Source/CAFGitHubSignInViewController.m
@@ -8,6 +8,13 @@
 
 #import "CAFGitHubSignInViewController.h"
 
+
+@interface NSString (NSStringAdditions)
++ (NSString *) base64StringFromData:(NSData *)data length:(int)length;
+@end
+
+
+
 @interface CAFGitHubSignInViewController () <UITextFieldDelegate, NSURLConnectionDataDelegate>
 @property (strong, nonatomic) IBOutlet UITextField *usernameField;
 @property (strong, nonatomic) IBOutlet UITextField *passwordField;
@@ -91,16 +98,26 @@ didReceiveResponse:(NSURLResponse *)response
 willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
 {
     if ([challenge previousFailureCount] < 1) {
-        if ([[challenge protectionSpace] authenticationMethod] == NSURLAuthenticationMethodServerTrust) {
-            [[challenge sender] rejectProtectionSpaceAndContinueWithChallenge:challenge];
-        } else {
+//        if ([[challenge protectionSpace] authenticationMethod] == NSURLAuthenticationMethodServerTrust) {
+//            [[challenge sender] rejectProtectionSpaceAndContinueWithChallenge:challenge];
+//        } else {
             NSURLCredential *credential;
-            credential = [NSURLCredential credentialWithUser:self.usernameField.text
-                                                    password:self.passwordField.text
-                                                 persistence:NSURLCredentialPersistenceForSession];
+//        NSString *username = [NSString base64StringFromData:[self.usernameField.text dataUsingEncoding:NSUTF8StringEncoding]
+//                                                     length:self.usernameField.text.length];
+
+        NSString *username = self.usernameField.text;
+
+//        NSString *password = [NSString base64StringFromData:[self.passwordField.text dataUsingEncoding:NSUTF8StringEncoding]
+//                                                        length:self.passwordField.text.length];
+        
+        NSString *password = self.passwordField.text;
+
+            credential = [NSURLCredential credentialWithUser:username
+                                                    password:password
+                                                 persistence:NSURLCredentialPersistenceNone];
             [[challenge sender] useCredential:credential
                    forAuthenticationChallenge:challenge];
-        }
+//        }
     } else {
         [[challenge sender] cancelAuthenticationChallenge:challenge];
         UIAlertView *alert;
@@ -165,7 +182,8 @@ willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challe
 {
     NSDictionary *authDict = @{
         @"scopes": @[@"gists"],
-        @"note": @"Curiosity App"
+        @"note": @"Curiosity App",
+        @"note_url": @"http://codecaffeine.com"
     };
     NSError *jsonEncodeError;
     NSData *jsonObject = [NSJSONSerialization dataWithJSONObject:authDict
@@ -173,11 +191,23 @@ willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challe
                                                            error:&jsonEncodeError];
         
     if (jsonObject) {
-        NSURL *url = [NSURL URLWithString:@"https://api.github.com/gists"];
-        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-//        [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-//        [request setHTTPMethod:@"POST"];
-//        [request setHTTPBody:jsonObject];
+        NSURL *url = [NSURL URLWithString:@"https://api.github.com/authorizations"];
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url
+                                                               cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
+                                                           timeoutInterval:30.0];
+        
+//        NSString *usernamePassword = [NSString stringWithFormat:@"%@:%@",
+//                                      self.usernameField.text,
+//                                      self.passwordField.text];
+//        NSString *encodedUsernamePassword = [NSString base64StringFromData:[usernamePassword dataUsingEncoding:NSUTF8StringEncoding]
+//                                                                    length:[usernamePassword length]];
+//        
+//        [request setValue:[NSString stringWithFormat:@"Basic %@", encodedUsernamePassword]
+//       forHTTPHeaderField:@"Authorization"];
+        
+        [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+        [request setHTTPMethod:@"POST"];
+        [request setHTTPBody:jsonObject];
         
         self.authenticationConnection = [NSURLConnection connectionWithRequest:request
                                                                       delegate:self];
@@ -190,6 +220,74 @@ willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challe
 - (IBAction)signInPressed:(UIButton *)sender
 {
     [self signIn];
+}
+
+@end
+
+
+static char base64EncodingTable[64] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+};
+
+@implementation NSString (NSStringAdditions)
+
++ (NSString *)base64StringFromData:(NSData *)data length:(int)length
+{
+    unsigned long ixtext, lentext;
+    long ctremaining;
+    unsigned char input[3], output[4];
+    short i, charsonline = 0, ctcopy;
+    const unsigned char *raw;
+    NSMutableString *result;
+    
+    lentext = [data length];
+    if (lentext < 1)
+        return @"";
+    result = [NSMutableString stringWithCapacity: lentext];
+    raw = [data bytes];
+    ixtext = 0;
+    
+    while (true) {
+        ctremaining = lentext - ixtext;
+        if (ctremaining <= 0)
+            break;
+        for (i = 0; i < 3; i++) {
+            unsigned long ix = ixtext + i;
+            if (ix < lentext)
+                input[i] = raw[ix];
+            else
+                input[i] = 0;
+        }
+        output[0] = (input[0] & 0xFC) >> 2;
+        output[1] = ((input[0] & 0x03) << 4) | ((input[1] & 0xF0) >> 4);
+        output[2] = ((input[1] & 0x0F) << 2) | ((input[2] & 0xC0) >> 6);
+        output[3] = input[2] & 0x3F;
+        ctcopy = 4;
+        switch (ctremaining) {
+            case 1:
+                ctcopy = 2;
+                break;
+            case 2:
+                ctcopy = 3;
+                break;
+        }
+        
+        for (i = 0; i < ctcopy; i++)
+            [result appendString: [NSString stringWithFormat: @"%c", base64EncodingTable[output[i]]]];
+        
+        for (i = ctcopy; i < 4; i++)
+            [result appendString: @"="];
+        
+        ixtext += 3;
+        charsonline += 4;
+        
+        if ((length > 0) && (charsonline >= length))
+            charsonline = 0;
+    }     
+    return result;
 }
 
 @end
