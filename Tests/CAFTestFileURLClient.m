09eb1032bcbcedd625337b361eb0a7ad8cf36426
Matt Thomas <matt@codecaffeine.com>
2013-04-01 06:08:57 -0400

    Oops, actually push the 404 test
    And fix a warning from AFNetowrking

--- Tests/CAFTestFileURLClient.m
+++ Tests/CAFTestFileURLClient.m
@@ -8,11 +8,13 @@
 
 #import "CAFTestFileURLClient.h"
 #import "CAFMockURLConnection.h"
+#import "CAFFileURLClient.h"
 
 @implementation CAFTestFileURLClient
 
 - (void)setUp
 {
+    [super setUp];
     [NSURLProtocol registerClass:[CAFMockURLConnection class]];
 }
 
@@ -20,12 +22,31 @@
 - (void)tearDown
 {
     [NSURLProtocol unregisterClass:[CAFMockURLConnection class]];
+    [super tearDown];
 }
 
 
 - (void)test404ReturnsError
 {
-    STFail(@"Not implemented");
+    [CAFMockURLConnection setResponseWithStatusCode:404
+                                       headerFields:nil
+                                           bodyData:nil];
+    
+    CAFFileURLClient *client = [[CAFFileURLClient alloc] initWithBaseURL:[NSURL URLWithString:@"http://foobar.com"]];
+    
+    __block NSError *returnedError;
+    [client getFileAtPath:@"something.txt"
+                  success:nil
+                  failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                      returnedError = error;
+                  }];
+    // Run loop
+    NSDate *until = [NSDate dateWithTimeIntervalSinceNow:0.5];
+    while (!returnedError && ([until compare:[NSDate date]] == NSOrderedDescending)) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
+                                 beforeDate:until];
+    }
+    STAssertNotNil(returnedError, @"Error should not be nil");
 }
 
 @end
