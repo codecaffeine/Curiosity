dfa06daf98f9b7e4a97dd07de585ddd36bb9de57
Matt Thomas <matt@codecaffeine.com>
2013-04-13 12:40:22 -0400

    Testing ViewModel

--- Tests/CAFTestMatchedTextViewModel.m
+++ Tests/CAFTestMatchedTextViewModel.m
@@ -7,7 +7,29 @@
 //
 
 #import "CAFTestMatchedTextViewModel.h"
+#import "CAFMatchedTextViewModel.h"
 
 @implementation CAFTestMatchedTextViewModel
+
+- (void)testSettingSourceTextAndRegexSendSignal {
+    CAFMatchedTextViewModel *viewModel = [CAFMatchedTextViewModel new];
+    __block BOOL signalCalled = NO;
+    [viewModel.matches subscribeNext:^(NSArray *matches) {
+        signalCalled = YES;
+	}];
+    
+    viewModel.sourceText = @"a url = http://codecaffene.com right?";
+    viewModel.regex = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypeLink
+                                                      error:nil];
+    STAssertNotNil(viewModel.regex, @"Why is the regex nil?");
+
+    // Run loop
+    NSDate *until = [NSDate dateWithTimeIntervalSinceNow:0.5];
+    while (!signalCalled && ([until compare:[NSDate date]] == NSOrderedDescending)) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
+                                 beforeDate:until];
+    }
+    STAssertTrue(signalCalled, @"signal should have been called");
+}
 
 @end
