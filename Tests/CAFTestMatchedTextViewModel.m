e1bbebb5d70e10d2951e4076a08ee70a7bb9e43c
Matt Thomas <matt@codecaffeine.com>
2013-04-13 12:26:23 -0400

    Adding CAFTEstMatchedTextViewController

---
+++ Tests/CAFTestMatchedTextViewModel.m
@@ -0,0 +1,13 @@
+//
+//  CAFTestMatchedTextViewModel.m
+//  Curiosity
+//
+//  Created by Matthew Thomas on 4/13/13.
+//  Copyright (c) 2013 Matthew Thomas. All rights reserved.
+//
+
+#import "CAFTestMatchedTextViewModel.h"
+
+@implementation CAFTestMatchedTextViewModel
+
+@end
