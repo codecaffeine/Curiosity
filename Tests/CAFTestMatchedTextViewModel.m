de3745afd58faebcfb13a951952a44fd73020454
Matt Thomas <matt@codecaffeine.com>
2013-04-13 12:46:38 -0400

    Added test for signal created after viewModel
    Also added a sanity test to make sure our regex is being called

--- Tests/CAFTestMatchedTextViewModel.m
+++ Tests/CAFTestMatchedTextViewModel.m
@@ -21,9 +21,11 @@
     viewModel.sourceText = @"a url = http://codecaffene.com right?";
     viewModel.regex = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypeLink
                                                       error:nil];
-    STAssertNotNil(viewModel.regex, @"Why is the regex nil?");
+    NSUInteger sanityCount = [viewModel.regex numberOfMatchesInString:viewModel.sourceText
+                                                              options:0
+                                                                range:NSMakeRange(0, viewModel.sourceText.length)];
+    STAssertTrue(sanityCount > 0, @"regex should have matches but has %u", sanityCount);
 
-    // Run loop
     NSDate *until = [NSDate dateWithTimeIntervalSinceNow:0.5];
     while (!signalCalled && ([until compare:[NSDate date]] == NSOrderedDescending)) {
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
@@ -31,5 +33,30 @@
     }
     STAssertTrue(signalCalled, @"signal should have been called");
 }
+
+
+- (void)testSettingSourceTextAndRegexBeforeSignalCreationSendSignal {
+    CAFMatchedTextViewModel *viewModel = [CAFMatchedTextViewModel new];
+    
+    viewModel.sourceText = @"a url = http://codecaffene.com right?";
+    viewModel.regex = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypeLink
+                                                      error:nil];
+    NSUInteger sanityCount = [viewModel.regex numberOfMatchesInString:viewModel.sourceText
+                                                              options:0
+                                                                range:NSMakeRange(0, viewModel.sourceText.length)];
+    STAssertTrue(sanityCount > 0, @"regex should have matches but has %u", sanityCount);
+    
+    __block BOOL signalCalled = NO;
+    [viewModel.matches subscribeNext:^(NSArray *matches) {
+        signalCalled = YES;
+	}];
+    NSDate *until = [NSDate dateWithTimeIntervalSinceNow:0.5];
+    while (!signalCalled && ([until compare:[NSDate date]] == NSOrderedDescending)) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
+                                 beforeDate:until];
+    }
+    STAssertTrue(signalCalled, @"signal should have been called");
+}
+
 
 @end
