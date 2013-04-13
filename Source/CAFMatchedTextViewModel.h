8fc570feacacf2c1dd955a6debd583eaff352252
Matt Thomas <matt@codecaffeine.com>
2013-04-13 12:40:17 -0400

    Stubbing ViewModel

--- Source/CAFMatchedTextViewModel.h
+++ Source/CAFMatchedTextViewModel.h
@@ -10,6 +10,13 @@
 #import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface CAFMatchedTextViewModel : NSObject
+@property (copy, nonatomic) NSString *sourceText;
 @property (copy, nonatomic) NSMutableString *text;
 @property (copy, nonatomic) NSRegularExpression *regex;
+
+/**
+ Creates and returns a signal for the matches are derived from applying source 
+ text against a regex
+ */
+- (RACSignal *)matches;
 @end
--- Source/CAFMatchedTextViewModel.m
+++ Source/CAFMatchedTextViewModel.m
@@ -10,4 +10,9 @@
 
 @implementation CAFMatchedTextViewModel
 
+- (RACSignal *)matches
+{
+    return nil;
+}
+
 @end
